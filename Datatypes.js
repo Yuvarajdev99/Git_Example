5e46e0432f2b510f43a27b8e007d2e17f0b48dac
yuvarajdev99 <yuvarajdev99@gmail.com>
2023-03-22 21:37:04 +0530

    Added files

---
+++ Datatypes.js
@@ -0,0 +1,15 @@
+// let apple = '1'
+// let orange = '2'
+
+// console.log(+apple + +orange)
+
+
+// alert(apple + orange)
+
+// alert('1'+2+2)
+
+alert(1+2+'2')
+
+//added command
+
+//added command at line 15
